baf7131e8b002618402f5cc61ef71cc36d8f0fb0
wechu07 <wechu017@gmail.com>
2022-04-07 00:19:34 +0300

    add first project

---
+++ 0x00-ES6_basic/2-arrow.js
@@ -0,0 +1,10 @@
+#!/usr/bin/env node
+export default function getNeighborhoodsList() {
+  this.sanFranciscoNeighborhoods = ['SOMA', 'Union Square'];
+
+  const self = this;
+  this.addNeighborhood = (newNeighborhood) => {
+    self.sanFranciscoNeighborhoods.push(newNeighborhood);
+    return self.sanFranciscoNeighborhoods;
+  };
+}
